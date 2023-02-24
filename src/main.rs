4f4431afb5ced14478de769f428c1211cc1d363b
mclean <mclean.cozine@gmail.com>
2023-02-24 15:59:28 -0500

    initial implementation of Ingredient, Step, and Recipe structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
     println!("Hello, world!");
 }
+
+struct Ingredient {
+    name: String,
+    amount: f32,
+    unit: String,
+    special_instructions: String,
+}
+
+struct Step {
+    order: u8,
+    description: String,
+}
+
+struct Recipe {
+    name: String,
+    description: String,
+    source: String,
+    ingredients: Vec<Ingredient>,
+    steps: Vec<Step>,
+}
