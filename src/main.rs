ae3b96cdebff70de2711921a6b8c22e1ab588a0a
mclean <mclean.cozine@gmail.com>
2023-03-02 17:57:33 -0500

    create methods to add steps to and get steps from a recipe

--- src/main.rs
+++ src/main.rs
@@ -12,3 +12,13 @@ struct Recipe {
     ingredients: Vec<recipe_components::Ingredient>,
     steps: Vec<recipe_components::Step>,
 }
+
+impl Recipe {
+    fn add_step(&mut self, recipe_components::Step step) {
+        self.steps.push(step);
+    }
+
+    fn get_step(&self, i32 step_index) -> Option<Step> {
+        self.steps.get(step_index)
+    }
+}
