46c0ca790cb86b4dba0f8abca5d66f3bed15fb44
mclean <mclean.cozine@gmail.com>
2023-04-06 16:56:36 -0400

    add getter and setter methods to Recipe impl

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,40 @@
-struct Recipe {
+pub struct Recipe {
     name: String,
     source: String,
     ingredients: Vec<String>,
 }
 
 impl Recipe {
-    fn new(name: &str) -> Self {
+    pub fn new(name: &str) -> Self {
         Self {
             name: name.to_string(),
             source: String::new(),
             ingredients: Vec::new(),
         }
+    }
+
+    pub fn get_name(&self) -> &str {
+        &self.name
+    }
+
+    pub fn add_source(&mut self, source: &str) {
+        self.source = source.to_string();
+    }
+
+    pub fn get_source(&self) -> &str {
+        &self.source
+    }
+
+    pub fn add_ingredient(&mut self, ingredient: &str) {
+        self.ingredients.push(ingredient.to_string());
+    }
+
+    pub fn get_ingredients(&self) -> Vec<&str> {
+        let mut result = Vec::new();
+        for ingredient in &self.ingredients {
+            result.push(&ingredient[..]);
+        }
+        result
     }
 }
 
@@ -21,5 +45,25 @@ mod tests {
     #[test]
     fn new_recipe() {
         let recipe = Recipe::new("test_recipe");
+        assert_eq!(recipe.get_name(), "test_recipe");
+    }
+
+    #[test]
+    fn adding_source() {
+        let mut recipe = Recipe::new("test_recipe");
+        recipe.add_source("test_source");
+        assert_eq!(recipe.get_source(), "test_source");
+    }
+
+    #[test]
+    fn adding_ingredients() {
+        let mut recipe = Recipe::new("test_recipe");
+        recipe.add_ingredient("salt");
+        let mut expected_result = Vec::new();
+        expected_result.push("salt");
+        assert_eq!(recipe.get_ingredients(), expected_result);
+        recipe.add_ingredient("pepper");
+        expected_result.push("pepper");
+        assert_eq!(recipe.get_ingredients(), expected_result);
     }
 }
