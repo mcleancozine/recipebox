dcc330dc8ab3c51d48e8d07cc4c7c320e362bacf
mclean <mclean.cozine@gmail.com>
2023-04-19 12:38:47 -0400

    Make name and source args optional

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
 use clap::Parser;
 use log::{debug};
 use recipe_box;
+use std::io;
 
-/// Simple program to take in and store a recipe with its source and ingredients
+/// A simple program to take in and store the source and ingredients of a recipe.
 #[derive(Parser)]
 struct Args {
     /// Name of the recipe 
     #[arg(short, long)]
-    name: String,
+    name: Option<String>,
 
     /// Source of the recipe (cookbook name, website, etc.)
     #[arg(short, long)]
-    source: String,
+    source: Option<String>,
     /*
     /// List of ingredients in the recipe
     #[arg(short, long)]
@@ -27,5 +28,45 @@ fn main() {
 
 fn run() {
     let args = Args::parse();
-    recipe_box::create_recipe(&args.name, &args.source);
+    let name = parse_name(args.name);
+    let source = parse_source(args.source);
+    recipe_box::create_recipe(&name[..], &source[..]);
+}
+
+fn parse_name(arg_name: Option<String>) -> String {
+    match arg_name {
+        Some(name) => name,
+        None => prompt_name()
+    }
+}
+
+fn parse_source(arg_source: Option<String>) -> String {
+    match arg_source {
+        Some(source) => source,
+        None => prompt_source()
+    }
+}
+
+fn prompt_name() -> String {
+    println!("Enter recipe name:");
+
+    let mut name = String::new();
+
+    io::stdin()
+        .read_line(&mut name)
+        .expect("Failed to read name");
+
+    name
+}
+
+fn prompt_source() -> String {
+    println!("Enter recipe source:");
+
+    let mut source = String::new();
+
+    io::stdin()
+        .read_line(&mut source)
+        .expect("Failed to read source");
+
+    source
 }
--- tests/cli.rs
+++ tests/cli.rs
@@ -3,31 +3,41 @@ use predicates::prelude::*; // Used for writing assertions
 use std::process::Command; // Run programs
 
 #[test]
-fn one_name_one_source() -> Result<(), Box<dyn std::error::Error>> {
+fn no_args() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin("recipe_box")?;
 
-    let assert = cmd.arg("-n").arg("test_name").arg("-s").arg("test_source").assert();
+    let assert = cmd.assert();
     assert.success();
 
     Ok(())
 }
 
 #[test]
-fn not_enough_args() -> Result<(), Box<dyn std::error::Error>> {
+fn only_name() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin("recipe_box")?;
 
     let assert = cmd.arg("-n").arg("test_name").assert();
-    assert.failure().stderr(predicate::str::contains("the following required arguments were not provided"));
+    assert.success();
 
     Ok(())
 }
 
 #[test]
-fn too_many_args() -> Result<(), Box<dyn std::error::Error>> {
+fn only_source() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin("recipe_box")?;
 
-    let assert = cmd.arg("-n").arg("test_name").arg("-s").arg("test_source").arg("extra_arg").assert();
-    assert.failure().stderr(predicate::str::contains("unexpected argument"));
+    let assert = cmd.arg("-s").arg("test_source").assert();
+    assert.success();
+
+    Ok(())
+}
+
+#[test]
+fn one_name_one_source() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("recipe_box")?;
+
+    let assert = cmd.arg("-n").arg("test_name").arg("-s").arg("test_source").assert();
+    assert.success();
 
     Ok(())
 }
@@ -48,6 +58,76 @@ fn multi_word_source() -> Result<(), Box<dyn std::error::Error>> {
 
     let assert = cmd.arg("-n").arg("test_name").arg("-s").arg("test source").assert();
     assert.success();
+
+    Ok(())
+}
+
+#[test]
+fn too_many_args() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("recipe_box")?;
+
+    let assert = cmd.arg("-n").arg("test_name").arg("-s").arg("test_source").arg("extra_arg").assert();
+    assert.failure().stderr(predicate::str::contains("unexpected argument"));
+
+    Ok(())
+}
+
+#[test]
+fn extra_name() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("recipe_box")?;
+
+    let assert = cmd.arg("-n").arg("test_name").arg("-s").arg("test_source").arg("-n").arg("extra_name").assert();
+    assert.failure().stderr(predicate::str::contains("cannot be used multiple times"));
+
+    Ok(())
+}
+
+#[test]
+fn extra_source() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("recipe_box")?;
+
+    let assert = cmd.arg("-n").arg("test_name").arg("-s").arg("test_source").arg("-s").arg("extra_source").assert();
+    assert.failure().stderr(predicate::str::contains("cannot be used multiple times"));
+
+    Ok(())
+}
+
+#[test]
+fn empty_name() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("recipe_box")?;
+
+    let assert = cmd.arg("-n").arg("").arg("-s").arg("test_source").assert();
+    assert.failure().stderr(predicate::str::contains("needs value"));
+
+    Ok(())
+}
+
+#[test]
+fn empty_source() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("recipe_box")?;
+
+    let assert = cmd.arg("-n").arg("test_name").arg("-s").arg("").assert();
+    assert.failure().stderr(predicate::str::contains("needs value"));
+
+    Ok(())
+}
+
+#[test]
+fn space_for_name() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("recipe_box")?;
+
+    let assert = cmd.arg("-n").arg(" ").arg("-s").arg("test_source").assert();
+    assert.failure().stderr(predicate::str::contains("needs value"));
+
+    Ok(())
+}
+
+#[test]
+fn space_for_source() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("recipe_box")?;
+
+    let assert = cmd.arg("-n").arg("test_name").arg("-s").arg(" ").assert();
+    assert.failure().stderr(predicate::str::contains("needs value"));
 
     Ok(())
 }
